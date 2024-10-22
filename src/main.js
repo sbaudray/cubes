72e00c54e6f2c72640795d942426877c884dc378
Mickael <mickael@lempire.co>
2024-10-22 09:43:18 +0200

    fir commit

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+
+if (Meteor.isClient) {
+
+  Template.world.onCreated(function() {
+
+  });
+
+  Template.world.helpers({
+
+  });
+
+  Template.world.events({
+
+  });
+
+}
+
+if (Meteor.isServer) {
+  
+}
